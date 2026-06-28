817ad8acb88fbab5391edc476ac0f5e005f21d33
Nguyễn Ngọc Minh Thư <23030035@student.bdu.edu.vn>
2026-06-28 15:52:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.image("logo.jpg")
 # TIÊU ĐỀ
 # =============================
 st.title("📈 TRỰC QUAN HÓA GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-st.subheader("NGUYỄN NGỌC MINH THƯ")
+st.subheader("NGUYỄN NGỌC MINH THƯ - ĐỀ TÀI 4")
 
 st.markdown("---")
 
